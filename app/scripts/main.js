79436ba671427164c41f96ade71783ab109a19f3
Maggie Martin <maggierozay@gmail.com>
2014-10-31 13:20:37 -0400

    commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,11 @@
-/*
+
 var Member = Backbone.Model.extend({
 
   defaults: {
     name: '',
     email: '',
-    mailing: true
+    username: '',
+    password: ''
   },
   initialize: function () {
     var n = this.get('name');
@@ -21,28 +22,55 @@ var Members = Backbone.Collection.extend ({
 });
 
 var all_members = new Members();
-*/
+
+/*var check = function(){
+
+
+if (document.Form1.checkbox1.checked == true)
+  {
+    (this).status === 'true';
+    console.log('true!!!');
+
+  }
+else if(document.Form1.checkbox1.checked == false)
+  {
+    (this).status === 'false';
+    console.log('false!!!');
+  }
+};*/
+$('#join-us').on('click', function (event){
+    event.preventDefault();
+    
+
+    $('.Join').fadeIn();
+
+});
 $('#memberForm').on('submit', function (event){
   event.preventDefault();
-/*/
-  e.preventDefault();
+
+
   var member_name = $('#name').val();
   var member_email = $('#email').val();
+
 
   var m = new Member({
     name: member_name,
     email: member_email
+
   });
+
+
+
 
   all_members.add(m);
 
   m.save();
 
-  console.log(m)*/
-  $('.hero-unit').fadeOut(300, function (){
-
-  });
+  console.log(m)
 
 
+
+  $('#name').val('');
+  $('#email').val('');
 
 });
